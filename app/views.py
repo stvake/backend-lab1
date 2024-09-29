f5baa050621f3dedb1474bfad207104687ef57a7
stvake <stvakemail@gmail.com>
2024-09-29 13:32:27 +0300

    Added code in init.py; Added endpoint healthcheck in views.py

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from app import app
+
+from flask import jsonify
+from datetime import datetime
+
+
+@app.route("/healthcheck", methods=["GET"])
+def healthcheck():
+    return jsonify(datetime.today())
