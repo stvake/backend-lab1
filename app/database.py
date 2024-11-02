974522f956d53d64ebba19a1b2fd175de5b141fa
stvake <stvakemail@gmail.com>
2024-11-02 16:30:30 +0200

    Lab2 - categories endpoints

--- app/database.py
+++ app/database.py
@@ -2,24 +2,33 @@ users = []
 categories = []
 records = []
 
-
 def add_user(user):
     users.append(user)
 
-
 def get_all_users():
     return users
-
 
 def get_user_by_id(user_id):
     for user in users:
         if user['id'] == user_id:
             return user
 
-
 def delete_user_by_id(user_id):
     for user in users:
         if user['id'] == user_id:
             users.remove(user)
+            return True
+    return False
+
+def add_category(category):
+    categories.append(category)
+
+def get_all_categories():
+    return categories
+
+def delete_category(category_id):
+    for category in categories:
+        if category['id'] == category_id:
+            categories.remove(category)
             return True
     return False
--- app/views.py
+++ app/views.py
@@ -4,7 +4,6 @@ import database as db
 from flask import jsonify, request
 from datetime import datetime
 
-
 @app.get("/healthcheck")
 def healthcheck():
     response = {
@@ -13,7 +12,6 @@ def healthcheck():
         "status": "ok"
     }
     return jsonify(response), 200
-
 
 @app.post("/user")
 def create_user():
@@ -25,11 +23,9 @@ def create_user():
     }
     return jsonify(response), 201
 
-
 @app.get("/users")
 def get_users():
     return jsonify(db.get_all_users()), 200
-
 
 @app.get("/user/<int:user_id>")
 def get_user(user_id):
@@ -38,9 +34,29 @@ def get_user(user_id):
         return jsonify(user), 200
     return jsonify({"message": "User not found"}), 404
 
-
 @app.delete("/user/<int:user_id>")
 def delete_user(user_id):
     if db.delete_user_by_id(user_id):
         return jsonify({"message": "User deleted"}), 200
     return jsonify({"message": "User not found"}), 404
+
+@app.post("/category")
+def create_category():
+    category = request.get_json()
+    db.add_category(category)
+    response = {
+        "message": "Category created",
+        "date": datetime.today(),
+        "status": "ok"
+    }
+    return jsonify(response), 201
+
+@app.get("/category")
+def get_categories():
+    return jsonify(db.get_all_categories()), 200
+
+@app.delete("/category/<int:category_id>")
+def delete_category(category_id):
+    if db.delete_category(category_id):
+        return jsonify({"message": "Category deleted"}), 200
+    return jsonify({"message": "Category not found"}), 404
