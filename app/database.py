56a5ee996f44339e47cce1d12e6168d726c1faea
stvake <stvakemail@gmail.com>
2024-11-02 16:24:13 +0200

    Lab2 - user endpoints

--- app/database.py
+++ app/database.py
@@ -1,3 +1,21 @@
 users = []
 categories = []
 records = []
+
+
+def get_all_users():
+    return users
+
+
+def get_user_by_id(user_id):
+    for user in users:
+        if user['id'] == user_id:
+            return user
+
+
+def delete_user_by_id(user_id):
+    for user in users:
+        if user['id'] == user_id:
+            users.remove(user)
+            return True
+    return False
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from app import app
 import database as db
 
-from flask import jsonify
+from flask import jsonify, request
 from datetime import datetime
 
 
@@ -13,3 +13,36 @@ def healthcheck():
         "status": "ok"
     }
     return jsonify(response), 200
+
+
+@app.post("/user")
+def create_user():
+    user = request.get_json()
+    db.users.append(user)
+
+    response = {
+        "message": "User created",
+        "date": datetime.today(),
+        "status": "ok"
+    }
+    return jsonify(response), 201
+
+
+@app.get("/users")
+def get_users():
+    return jsonify(db.get_all_users()), 200
+
+
+@app.get("/user/<int:user_id>")
+def get_user(user_id):
+    user = db.get_user_by_id(user_id)
+    if user:
+        return jsonify(user), 200
+    return jsonify({"message": "User not found"}), 404
+
+
+@app.delete("/user/<int:user_id>")
+def delete_user(user_id):
+    if db.delete_user_by_id(user_id):
+        return jsonify({"message": "User deleted"}), 200
+    return jsonify({"message": "User not found"}), 404
